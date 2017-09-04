628aed22e032e89b5ce9d8a10ed0c0bbcb17a714
peter.arnold <peter.arnold@cpcconnect.co.uk>
2017-09-04 17:18:57 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var data = {
+    "stories": [
+        {
+            "id": 1,
+            "headline": "Most Important Story",
+            "subline": "This article has the most visual weight.",
+            "hero": "images/hero.jpg",
+            "source": "https://pixabay.com/en/lamp-light-hand-idea-lights-2663053/"
+        },
+        {
+            "id": 2,
+            "headline": "Less Important Story",
+            "subline": "This story has less visual weight.",
+            "hero": "http://placekitten.com/250/350",
+        },
+        {
+            "id": 3,
+            "headline": "Less Important Story The Second",
+            "subline": "This story has less visual weight.",
+            "hero": "http://placecage.com/250/350",
+            "source": "http://placecage.com"
+        },
+        {
+            "id": 4,
+            "headline": "The Third Less Important Story",
+            "subline": "This story has less visual weight.",
+            "hero": "http://placebear.com/250/350"
+        }
+    ]
+};
---
+++ readme.md
@@ -0,0 +1,5 @@
+# CPC Frontend Developer Task
+
+1. Update the CSS to allow the page to look good on both mobiles and desktops.
+2. Load the articles from the JSON object provided in script.js.
+3. Validate the subscription form.
